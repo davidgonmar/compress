a965dec5cf0963e797d4f69ef7ead66f29f2a8fd
David González Martínez <me@davidgonmar.me>
2025-02-09 05:56:06 +0100

    feat: add offline quant

--- compress/quant.py
+++ compress/quant.py
@@ -425,6 +425,88 @@ def to_quantized_online(
     return model
 
 
+def get_activations(model, data_loader):
+    activations = {}
+    hooks = []
+    for _, module in gather_submodules(model, should_do=default_should_do, prefix=""):
+        activations[module] = []
+
+        def hook_fn(activations):
+            def _hook_fn(module, input, output):
+                activations.append(output.detach().cpu())
+
+            return _hook_fn
+
+        hooks.append(module.register_forward_hook(hook_fn(activations[module])))
+
+    for element in data_loader:
+        model(element)
+
+    for hook in hooks:
+        hook.remove()
+
+    # cat activations
+    for module in activations:
+        activations[module] = torch.cat(activations[module], dim=0)
+    return activations
+
+
+def to_quantized_offline(
+    model: nn.Module,
+    input_specs: IntQuantizationSpec,
+    weight_specs: IntQuantizationSpec,
+    inplace=True,
+    should_do=default_should_do,
+    data_loader=None,
+    activations=None,
+    **kwargs,
+):
+    # only one of data_loader and activations should be provided
+    assert (data_loader is None) != (
+        activations is None
+    ), "Either data_loader or activations should be provided"
+
+    if activations is None:
+        # first, estimate the quantization parameters with hooks
+        layer_and_input_acts = get_activations(model, data_loader)
+
+    else:
+        layer_and_input_acts = activations
+
+    input_infos = {}
+    for module, input_act in layer_and_input_acts.items():
+        input_infos[module] = calibrate(
+            input_act,
+            input_specs["linear" if isinstance(module, nn.Linear) else "conv2d"],
+        )
+
+    modules_to_replace = gather_submodules(model, should_do=should_do, prefix="")
+    if not inplace:
+        model_initializer = kwargs.pop("model_initializer", None)
+        assert (
+            model_initializer is not None
+        ), "model_initializer must be provided if inplace=False"
+        model_ = model_initializer()
+        model_.load_state_dict(model.state_dict())
+        model = model_
+
+    for name, module in tqdm(modules_to_replace, desc="Replacing modules"):
+        parent_module = model
+        *parent_path, attr_name = name.split(".")
+        for part in parent_path:
+            parent_module = getattr(parent_module, part)
+
+        setattr(
+            parent_module,
+            attr_name,
+            QuantizedLinear(weight_specs["linear"], input_infos[module], module)
+            if isinstance(module, nn.Linear)
+            else QuantizedConv2d(weight_specs["conv2d"], input_infos[module], module),
+        )
+
+    return model
+
+
 def prepare_for_qat(
     model: nn.Module,
     input_specs: IntQuantizationSpec,
--- examples/quantize.py
+++ examples/quantize.py
@@ -1,7 +1,12 @@
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
-from compress.quant import IntQuantizationSpec, to_quantized_online
+from compress.quant import (
+    IntQuantizationSpec,
+    to_quantized_online,
+    to_quantized_offline,
+    get_activations,
+)
 import argparse
 import copy
 from itertools import product
@@ -10,7 +15,7 @@ from itertools import product
 parser = argparse.ArgumentParser()
 parser.add_argument("--save_path", type=str, default="qat_resnet.pth")
 parser.add_argument("--print_model", action="store_true")
-parser.add_argument("--dataset", type=str, default="mnist")
+parser.add_argument("--offline", action="store_true")
 args = parser.parse_args()
 
 
@@ -41,10 +46,8 @@ transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
 )
 
-test_dataset = (
-    datasets.MNIST(root="data", train=False, transform=transform, download=True)
-    if args.dataset == "mnist"
-    else datasets.CIFAR10(root="data", train=False, transform=transform, download=True)
+test_dataset = datasets.CIFAR10(
+    root="data", train=False, transform=transform, download=True
 )
 test_loader = DataLoader(test_dataset, batch_size=512, shuffle=False)
 
@@ -62,6 +65,31 @@ print(f"Test Loss: {loss[0]}, Test Accuracy: {loss[1]}")
 bit_widths = [8, 16]
 signed_options = [True]
 
+train_dataset = datasets.CIFAR10(
+    root="./data", train=True, download=True, transform=transform
+)
+
+torch.manual_seed(0)
+
+
+class OnlyImages:
+    def __init__(self, dataset):
+        self.dataset = [dataset[i] for i in torch.randperm(len(dataset))[: 512 * 10]]
+
+    def __iter__(self):
+        for x, _ in self.dataset:
+            yield x
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        return self.dataset[idx][0].to(device)
+
+
+dataloader = DataLoader(OnlyImages(train_dataset), batch_size=512, shuffle=True)
+
+activations = get_activations(model, dataloader)
 
 for w_linear_bits, w_conv_bits, i_linear_bits, i_conv_bits in product(
     bit_widths, bit_widths, bit_widths, bit_widths
@@ -76,13 +104,23 @@ for w_linear_bits, w_conv_bits, i_linear_bits, i_conv_bits in product(
         "conv2d": IntQuantizationSpec(i_conv_bits, signed_options[0]),
     }
 
-    quanted = to_quantized_online(
-        model,
-        inpspecs,
-        wspecs,
-        inplace=False,
-        model_initializer=lambda: copy.deepcopy(model),
-    )
+    if args.offline:
+        quanted = to_quantized_offline(
+            model,
+            inpspecs,
+            wspecs,
+            inplace=False,
+            model_initializer=lambda: copy.deepcopy(model),
+            activations=activations,
+        )
+    else:
+        quanted = to_quantized_online(
+            model,
+            inpspecs,
+            wspecs,
+            inplace=False,
+            model_initializer=lambda: copy.deepcopy(model),
+        )
     loss, accuracy = evaluate(quanted, test_loader, criterion, device)
     print(
         f"Weight Linear Bits: {w_linear_bits}, Weight Conv2D Bits: {w_conv_bits}, "
