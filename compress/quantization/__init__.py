fe697ed6c9015745f13e9d6adeee13c36822c218
David González Martínez <me@davidgonmar.me>
2025-05-21 22:07:37 +0200

    fix: calib

--- compress/quantization/__init__.py
+++ compress/quantization/__init__.py
@@ -44,6 +44,7 @@ from compress.common import (
     keys_passlist_should_do,
 )
 
+
 from compress.layer_fusion import fuse_batch_norm_inference, fuse_conv_bn
 from torch import nn
 from tqdm import tqdm
@@ -332,23 +333,17 @@ def requantize_qat(
         if hasattr(module, "weight_spec"):
             module.weight_spec = specs[name]["weight"]
         if hasattr(module, "weight_info"):
-            # re-scale
-            old_nbits = module.weight_info.spec.nbits
-            new_nbits = specs[name]["weight"].nbits
-            ratio = (2**old_nbits - 1) / (2**new_nbits - 1)
-            module.weight_info.scale = nn.Parameter(
-                module.weight_info.scale * ratio
-            ).requires_grad_(True)
-            module.weight_info.spec = specs[name]["weight"]
+            # calibrate
+            module.weight_info = calibrate(
+                module.weight,
+                specs[name]["weight"],
+            )
         if hasattr(module, "input_info"):
-            # re-scale
-            old_nbits = module.input_info.spec.nbits
-            new_nbits = specs[name]["input"].nbits
-            ratio = (2**old_nbits - 1) / (2**new_nbits - 1)
-            module.input_info.scale = nn.Parameter(
-                module.input_info.scale * ratio
-            ).requires_grad_(True)
-            module.input_info.spec = specs[name]["input"]
+            # calibrate
+            module.input_info = calibrate(
+                module.input,
+                specs[name]["input"],
+            )
 
     return model
 
